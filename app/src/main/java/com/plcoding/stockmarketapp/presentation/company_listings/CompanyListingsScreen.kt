339318941339d2eef24d1692c58db33af75f38b7
Leu Pashkouski <satyd@mail.ru>
2023-07-22 16:19:01 +0300

    finish with the canvas and drawings, apps done!!!

--- app/src/main/java/com/plcoding/stockmarketapp/presentation/company_listings/CompanyListingsScreen.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/presentation/company_listings/CompanyListingsScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import com.plcoding.stockmarketapp.presentation.destinations.CompanyInfoScreenDestination
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
@@ -63,9 +64,9 @@ fun CompanyListingsScreen(
                         modifier = Modifier
                             .fillMaxWidth()
                             .clickable {
-                                /*navigator.navigate(
+                                navigator.navigate(
                                     CompanyInfoScreenDestination(company.symbol)
-                                )*/
+                                )
                             }
                             .padding(16.dp)
                     )
